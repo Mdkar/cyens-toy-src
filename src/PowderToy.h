bef16ccb819e54a1121ab25b3fd4d88c04f09da2
firefreak11 <dave.yognaught113@gmail.com>
2020-03-08 01:20:57 -0500

    Purge

--- src/PowderToy.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "common/String.h"
-
-void EngineProcess();
-void ClipboardPush(ByteString text);
-ByteString ClipboardPull();
-int GetModifiers();
-unsigned int GetTicks();
--- src/gui/interface/Mouse.h
+++
@@ -1,6 +0,0 @@
-
-#ifdef SDL_INC
-#include "SDL2/SDL_mouse.h"
-#else
-#include "SDL_mouse.h"
-#endif
